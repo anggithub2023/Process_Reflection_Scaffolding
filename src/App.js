f6f900e1f07630dd364332f10e299add5c2595d8
anggithub2023 <147742090+anggithub2023@users.noreply.github.com>
2025-04-20 15:45:29 -0400

    button changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ReflectionPage from './pages/ReflectionPage';
+import ReadinessPage from './pages/ReadinessPage';
 import StatsPage from './pages/StatsPage';
 import VideosPage from './pages/VideosPage';
 import WorkoutsPage from './pages/WorkoutsPage';
@@ -11,6 +12,7 @@ function App() {
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/reflect" element={<ReflectionPage />} />
+            <Route path="/readiness" element={<ReadinessPage />} />
             <Route path="/stats" element={<StatsPage />} />
             <Route path="/videos" element={<VideosPage />} />
             <Route path="/workouts" element={<WorkoutsPage />} />
