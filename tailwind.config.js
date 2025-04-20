a594549418aa81f35c7c722e59b74aac4bb36b61
anggithub2023 <147742090+anggithub2023@users.noreply.github.com>
2025-04-19 23:24:28 -0400

    button changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-up': {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'fade-up': 'fade-up 0.8s ease-out forwards',
+      },
+    },
   },
   plugins: [],
-};
+}
